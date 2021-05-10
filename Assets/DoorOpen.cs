fdba5b36c1bab8ae519a6b0fba7558438c5cf2d6
Carter C <cclements3150@gmail.com>
2021-05-10 16:16:01 -0400

    Added basic door and projector interaction

---
+++ Assets/DoorOpen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    public void Interact()
+    {
+        if (anim.GetBool("character_nearby") == false)
+            anim.SetBool("character_nearby", true);
+        else
+            anim.SetBool("character_nearby", false);
+    }
+
+
+}
---
+++ Assets/ProjecterInteract.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjecterInteract : MonoBehaviour
+{
+    public GameObject projector;
+
+    public void interact()
+    {
+        if (projector.activeSelf == true)
+            projector.SetActive(false);
+        else
+            projector.SetActive(true);
+    }
+}
