55c133ef638b338fdf3ffbf3a70a7cabfbb47a9c
Carter C <cclements3150@gmail.com>
2021-05-11 23:02:11 -0400

    Added puzzles to each rooms

--- Assets/ProjecterInteract.cs
+++ Assets/ProjecterInteract.cs
@@ -6,16 +6,36 @@ public class ProjecterInteract : MonoBehaviour
 {
     public GameObject projector;
 
-    private bool unlocked = false;
+    private bool puzzle_complete = false;
+    public bool unlocked = false;
 
-    public void unlock()
+    public GameObject p1;
+    public GameObject p2;
+    public GameObject p3;
+    //public GameObject p2;
+
+    public void Update()
     {
-        unlocked = true;
-        interact();
+        if (puzzle_complete == false)
+        {
+            if (p1.GetComponent<ShapePuzzle>().isComplete
+                && p2.GetComponent<ShapePuzzle>().isComplete
+                && p3.GetComponent<ShapePuzzle>().isComplete
+            // && p4.GetComponent<ShapePuzzle>().isComplete
+            )
+            {
+                unlocked = true;
+                interact();
+                puzzle_complete = true;
+            }
+            else
+                unlocked = false;            
+        }
     }
 
     public void interact()
     {
+
         if (unlocked == true)
         {
             if (projector.activeSelf == true)
---
+++ Assets/ShapePuzzle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapePuzzle : MonoBehaviour
+{
+
+    public Renderer light;
+    public Material done;
+    public Material empty;
+
+    public bool isComplete = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "key")
+        {
+            light.material = done;
+            isComplete = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "key")
+        {
+            light.material = empty;
+            isComplete = false;
+        }
+    }
+}
